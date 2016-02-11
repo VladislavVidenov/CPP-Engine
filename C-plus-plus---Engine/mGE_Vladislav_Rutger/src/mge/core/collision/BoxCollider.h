e5db901daec04c6f3938360ac9d3c91e9acb2762
VladislavVidenov <vladislavvidenov95@gmail.com>
2016-02-11 14:46:37 +0100

    v 0.0004

--- C-plus-plus---Engine/mGE_Vladislav_Rutger/src/mge/core/collision/BoxCollider.h
+++ C-plus-plus---Engine/mGE_Vladislav_Rutger/src/mge/core/collision/BoxCollider.h
@@ -1,6 +1,6 @@
 #ifndef BOXCOLLIDER_H
 #define BOXCOLLIDER_H
-
+#include <glm.hpp>
 #include <Collider.h>
 
 
@@ -10,12 +10,17 @@ class BoxCollider : public Collider
         BoxCollider();
         virtual ~BoxCollider();
 
+    void collide(Collider * pCollider);
+    void collide(BoxCollider * pBoxCol);
+    void collide(SphereCollider * pSphereCol);
 
-        void collide(Collider * pCollider);
-        void collide(BoxCollider * pBoxCol);
-        void collide(SphereCollider * pSphereCol);
+    inline const glm::vec3 getMinBounds() const { return _minBounds; }
+    inline const glm::vec3 getMaxBounds() const { return _maxBounds; }
+
     protected:
     private:
+        glm::vec3 _minBounds;
+        glm::vec3 _maxBounds;
 };
 
 #endif // BOXCOLLIDER_H
